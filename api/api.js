509e3c1a2f61a9eb5012f86e91d8600dd4cd6599
Robin Buschmann <cm@Robins-MacBook-Pro.fritz.box>
2015-10-18 19:19:47 +0200

    js files removed from repository

--- api/api.js
+++
@@ -1,7 +0,0 @@
-///<reference path="../typings/node/node.d.ts"/>
-var ApiUnus_1 = require('./ApiUnus');
-exports.__esModule = true;
-exports["default"] = {
-    v1: new ApiUnus_1.ApiUnus()
-};
-//# sourceMappingURL=api.js.map
