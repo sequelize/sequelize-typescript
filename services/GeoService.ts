da3f9fdfbdadb6324f359417bc180d6293006203
Robin Buschmann <Robin@Robins-MacBook-Pro-2.local>
2016-09-29 09:25:10 +0200

    cluster epsilon further adjustment

--- services/GeoService.ts
+++ services/GeoService.ts
@@ -34,18 +34,18 @@ export class GeoService {
   getEpsilonByZoom(zoom: number) {
 
     if(zoom > 16){
-      return 0.0007;
+      return 0.0003;
     }
 
     switch (zoom) {
       case 16:
-        return 0.0008;
+        return 0.0005;
       case 15:
-        return 0.001;
+        return 0.0008;
       case 14:
-        return 0.0025;
+        return 0.001;
       case 13:
-        return 0.005;
+        return 0.0025;
       case 12:
         return 0.008;
       case 11:
