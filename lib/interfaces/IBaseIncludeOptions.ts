3e0ea5e8a3c9206a77496994d2ed6116ad3ad313
RobinBuschmann <robinbuschmann@hotmail.de>
2017-07-20 21:24:18 +0200

    closes #61 (fixes type declarations for attributes options)

---
+++ lib/interfaces/IBaseIncludeOptions.ts
@@ -0,0 +1,36 @@
+import {WhereOptions, IncludeThroughOptions, FindOptionsAttributesArray} from 'sequelize';
+
+/**
+ * Complex include options
+ */
+export interface IBaseIncludeOptions {
+
+  /**
+   * The alias of the relation, in case the model you want to eagerly load is aliassed. For `hasOne` /
+   * `belongsTo`, this should be the singular name, and for `hasMany`, it should be the plural
+   */
+  as?: string;
+
+  /**
+   * Where clauses to apply to the child models. Note that this converts the eager load to an inner join,
+   * unless you explicitly set `required: false`
+   */
+  where?: WhereOptions;
+
+  /**
+   * A list of attributes to select from the child model
+   */
+  attributes?: FindOptionsAttributesArray | { include?: FindOptionsAttributesArray, exclude?: string[] };
+
+  /**
+   * If true, converts to an inner join, which means that the parent model will only be loaded if it has any
+   * matching children. True if `include.where` is set, false otherwise.
+   */
+  required?: boolean;
+
+  /**
+   * Through Options
+   */
+  through?: IncludeThroughOptions;
+
+}
--- lib/interfaces/IScopeIncludeOptions.ts
+++ lib/interfaces/IScopeIncludeOptions.ts
@@ -1,11 +1,11 @@
-import {WhereOptions, IncludeThroughOptions} from 'sequelize';
 import {IScopeIncludeAssociation} from "./IScopeIncludeAssociation";
 import {ModelClassGetter} from "../types/ModelClassGetter";
+import {IBaseIncludeOptions} from "./IBaseIncludeOptions";
 
 /**
  * Complex include options
  */
-export interface IScopeIncludeOptions {
+export interface IScopeIncludeOptions extends IBaseIncludeOptions {
 
   /**
    * The model you want to eagerly load
@@ -13,37 +13,9 @@ export interface IScopeIncludeOptions {
   model?: ModelClassGetter;
 
   /**
-   * The alias of the relation, in case the model you want to eagerly load is aliassed. For `hasOne` /
-   * `belongsTo`, this should be the singular name, and for `hasMany`, it should be the plural
-   */
-    as?: string;
-
-  /**
    * The association you want to eagerly load. (This can be used instead of providing a model/as pair)
    */
   association?: IScopeIncludeAssociation;
-
-  /**
-   * Where clauses to apply to the child models. Note that this converts the eager load to an inner join,
-   * unless you explicitly set `required: false`
-   */
-  where?: WhereOptions;
-
-  /**
-   * A list of attributes to select from the child model
-   */
-  attributes?: string[];
-
-  /**
-   * If true, converts to an inner join, which means that the parent model will only be loaded if it has any
-   * matching children. True if `include.where` is set, false otherwise.
-   */
-  required?: boolean;
-
-  /**
-   * Through Options
-   */
-  through?: IncludeThroughOptions;
 
   /**
    * Load further nested related models
