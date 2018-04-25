b3779a1de277cbab7ed8e5a93b0b1a223a7be076
Johannes Scharlach <johannes@shareiq.com>
2018-04-25 09:20:09 +0200

    feat: allow setting unique constraint keys in decorator

--- lib/annotations/Unique.ts
+++ lib/annotations/Unique.ts
@@ -1,12 +1,27 @@
 import 'reflect-metadata';
 import {addAttributeOptions} from "../services/models";
 
-/**
- * Sets unique option true for annotated property.
- */
-export function Unique(target: any, propertyName: string): void {
+export type uniqueAnnotation = (target: any, propertyName: string) => void;
 
-  addAttributeOptions(target, propertyName, {
+/**
+ * Sets unique option for annotated property.
+ */
+export function Unique(keyCombinator: string): uniqueAnnotation;
+export function Unique(target: any, propertyName: string): void;
+export function Unique(targetOrCombinator: string | any, propertyName?: string): void | uniqueAnnotation  {
+  if (typeof targetOrCombinator === 'string') {
+    return (target: any, innerPropertyName: string) => {
+      addAttributeOptions(target, innerPropertyName, {
+        unique: targetOrCombinator
+      });
+    };
+  }
+
+  if (propertyName === undefined) {
+    throw Error('Property name needs to be defined');
+  }
+
+  addAttributeOptions(targetOrCombinator, propertyName, {
     unique: true
   });
 }
