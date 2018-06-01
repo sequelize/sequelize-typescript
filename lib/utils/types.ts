bf2d05253bdb6b78900319dd9519c18a846ffcc4
RobinBuschmann <robinbuschmann@hotmail.de>
2018-06-01 14:50:58 +0200

    Fixes #379

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,4 +1,5 @@
-export type Diff<T extends string, U extends string> = ({[P in T]: P} & {[P in U]: never} & { [x: string]: never })[T];
+export type Diff<T extends string | symbol | number, U extends string | symbol | number> =
+  ({[P in T]: P} & {[P in U]: never} & { [x: string]: never })[T];
 
 export type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
 
