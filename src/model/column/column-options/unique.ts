c1fc109ed447a25c32c6801896005d4a765b7c99
robinbuschmann <robinbuschmann@hotmail.de>
2019-02-26 16:54:12 +0100

    Resolves #363 and #176

--- src/model/column/column-options/unique.ts
+++ src/model/column/column-options/unique.ts
@@ -1,11 +1,30 @@
 import {addAttributeOptions} from "../attribute-service";
 
-/**
- * Sets unique option true for annotated property.
- */
-export function Unique(target: any, propertyName: string): void {
+type UniqueOptions = boolean | string | { name: string; msg: string };
 
+/**
+ * Sets unique option as specified in options and returns decorator
+ */
+export function Unique(options: UniqueOptions): Function;
+
+/**
+ * Decorator, which sets unique option true for annotated property.
+ */
+export function Unique(target: Object, propertyName: string): void;
+
+export function Unique(...args: any[]): void | Function {
+  if (args.length === 1) {
+    const [options] = args;
+    return (target, propertyName) => {
+      annotate(target, propertyName, options);
+    };
+  }
+  const [target, propertyName] = args;
+  annotate(target, propertyName);
+}
+
+function annotate(target: Object, propertyName: string, option: UniqueOptions = true) {
   addAttributeOptions(target, propertyName, {
-    unique: true
+    unique: option,
   });
 }
---
+++ test/specs/unique.spec.ts
@@ -0,0 +1,42 @@
+import {getAttributes, Model, Sequelize} from "../../src";
+import {Op} from "sequelize";
+import {expect} from "chai";
+import {Table} from "../../src/model/table/table";
+import {Column} from "../../src/model/column/column";
+import {Unique} from "../../src/model/column/column-options/unique";
+
+describe('unique decorator', () => {
+
+  let sequelize;
+  let User;
+
+  before(() => {
+    @Table
+    class UserModel extends Model<UserModel> {
+      @Unique('test') @Column name: string;
+      @Unique @Column key: string;
+    }
+    User = UserModel;
+
+    sequelize = new Sequelize({
+      operatorsAliases: Op,
+      dialect: 'sqlite',
+      storage: ':memory:',
+      logging: !('SEQ_SILENT' in process.env),
+      models: [User],
+    });
+  });
+
+  it('should set advanced unique options', () => {
+    const attributes = getAttributes(User.prototype);
+    expect(attributes).to.have.property('name')
+      .which.has.property('unique', 'test');
+  });
+
+  it('should enable unique options', () => {
+    const attributes = getAttributes(User.prototype);
+    expect(attributes).to.have.property('key')
+      .which.has.property('unique', true);
+  });
+
+});
