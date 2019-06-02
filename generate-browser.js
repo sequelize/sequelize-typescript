62decf516fb3501b4b9c722dc99ea8e7f531f995
Fabian Wiles <fabianwiles@live.com>
2019-06-02 20:00:39 +1200

    add buidl step and windows support

--- generate-browser.js
+++ generate-browser.js
@@ -6,13 +6,16 @@ const base = 'lib';
 const targetBase = 'browser';
 
 async function main() {
-  const annotationsDir = path.join(base, 'annotations');
+  const annotationsDir = path.posix.join(base, 'annotations');
   // gathering annotation inputs
-  const inputs = (await recursive(annotationsDir)).filter(f => f.endsWith('.js'));
+  const inputs = (await recursive(annotationsDir))
+    .filter(f => f.endsWith('.js'))
+    // since recursive-readdir dosen't provide an a posix api, replace all \\ with // 
+    .map(p => p.replace(/\\/g, '/'));
 
   // write out the noop function that all the annotations will use
-  const noopDir = path.join(targetBase, 'noop.js');
-  await mkdirp(path.dirname(noopDir));
+  const noopDir = path.posix.join(targetBase, 'noop.js');
+  await mkdirp(path.posix.dirname(noopDir));
   fs.writeFileSync(noopDir, `export function noop(){}`);
   const exportedFiles = [];
 
@@ -22,43 +25,43 @@ async function main() {
     // assume the function exported is the same as the filename
     const functionName = fileParts[fileParts.length -1].replace('.js', '');
 
-    const outPath = path.join(targetBase, f);
+    const outPath = path.posix.join(targetBase, f);
     exportedFiles.push({functionName, path: outPath});
-    const noopRelatitiveDir = path.relative(path.dirname(outPath), path.dirname(noopDir));
+    const noopRelatitiveDir = path.posix.relative(path.posix.dirname(outPath), path.posix.dirname(noopDir));
 
-    const file = `import {noop} from '${path.join(noopRelatitiveDir, 'noop')}';
+    const file = `import {noop} from '${path.posix.join(noopRelatitiveDir, 'noop')}';
 export function ${functionName}() { return noop; }
 `
 
-    await mkdirp(path.dirname(outPath));
+    await mkdirp(path.posix.dirname(outPath));
     fs.writeFileSync(outPath, file)
   }
 
   // we need to include the model file
-  const modelFilePath = path.join(targetBase, 'models', 'Model.js')
-  await mkdirp(path.dirname(modelFilePath));
+  const modelFilePath = path.posix.join(targetBase, 'models', 'Model.js')
+  await mkdirp(path.posix.dirname(modelFilePath));
   fs.writeFileSync(modelFilePath, `export class Model {}\n`)
   exportedFiles.push({functionName: 'Model', path: modelFilePath})
 
   // export all the files we made in our index
   let indexFile = '';
   for(const fn of exportedFiles) {
-    const p = path.relative(targetBase, fn.path).replace('.js', '')
+    const p = path.posix.relative(targetBase, fn.path).replace('.js', '')
     indexFile += `export {${fn.functionName}} from './${p}'\n`;
   }
-  fs.writeFileSync(path.join(targetBase, 'index.js'), indexFile);
+  fs.writeFileSync(path.posix.join(targetBase, 'index.js'), indexFile);
 
   // update package.json with new set of browser files
   const browser = {
-    "index.js": path.join(targetBase, "index.js"),
+    "index.js": path.posix.join(targetBase, "index.js"),
   };
   for(const fn of exportedFiles) {
-    const relativePath = path.relative(targetBase, fn.path);
+    const relativePath = path.posix.relative(targetBase, fn.path);
     browser[relativePath] = fn.path;
   }
-  const packjageJson = JSON.parse(fs.readFileSync('package.json').toString());
-  packjageJson.browser = browser;
-  fs.writeFileSync('package.json', JSON.stringify(packjageJson, null, 2))
+  const packageJson = JSON.parse(fs.readFileSync('package.json').toString());
+  packageJson.browser = browser;
+  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2))
 
 }
 
