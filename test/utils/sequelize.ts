f896a6f91aab34a0f2f80438d9b6bddd382ef961
Lukas Hroch <223967+lukashroch@users.noreply.github.com>
2022-02-11 17:45:19 +0000

    build(deps-dev): bump dev dependencies (#1205)

--- test/utils/sequelize.ts
+++ test/utils/sequelize.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from '../../src/sequelize/sequelize/sequelize';
-import { ModelOptions, Op } from 'sequelize';
+import { ModelOptions } from 'sequelize';
 import { SequelizeOptions } from '../../src/sequelize/sequelize/sequelize-options';
 
 export function createSequelize(partialOptions: Partial<SequelizeOptions>): Sequelize;
