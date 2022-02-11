ab45c14da8cbd388f7611c0703e1f198e1f4541b
Sang myoung <59010379+mystylelee@users.noreply.github.com>
2022-02-11 21:10:40 +0900

    Fix sequelize/types/lib/hooks path (#1202)
    * build(deps-dev): bump sequelize from 6.8.0 to 6.16.1
    
    Bumps [sequelize](https://github.com/sequelize/sequelize) from 6.8.0 to 6.16.1.
    - [Release notes](https://github.com/sequelize/sequelize/releases)
    - [Commits](https://github.com/sequelize/sequelize/compare/v6.8.0...v6.16.1)
    
    ---
    updated-dependencies:
    - dependency-name: sequelize
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix:  Cannot find module 'sequelize/types/lib/hooks'  wrong path fix
    
    'types/lib' folder was removed in sequelize verstion >= 6
    
    * test: [test/specs/instance.spec.ts] - Model.reload() method is return Model,
    Deprecated: Operator Aliases
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: mystylelee <mystylelee2@gmail.com>

--- test/utils/sequelize.ts
+++ test/utils/sequelize.ts
@@ -17,7 +17,6 @@ export function createSequelize(
   }
 
   return new Sequelize({
-    operatorsAliases: Op,
     database: '__',
     dialect: 'sqlite' as const,
     username: 'root',
@@ -32,7 +31,6 @@ export function createSequelize(
 
 export function createSequelizeValidationOnly(useModelsInPath = true): Sequelize {
   return new Sequelize({
-    operatorsAliases: Op,
     validateOnly: true,
     logging: !('DISABLE_LOGGING' in process.env),
     models: useModelsInPath ? [__dirname + '/../models'] : [],
@@ -48,7 +46,6 @@ export function createSequelizeFromUri(useModelsInPath = true): Sequelize {
 
 export function createSequelizeFromUriObject(useModelsInPath = true): Sequelize {
   return new Sequelize('sqlite://', {
-    operatorsAliases: Op,
     modelPaths: useModelsInPath ? [__dirname + '/../models'] : [],
   });
 }
