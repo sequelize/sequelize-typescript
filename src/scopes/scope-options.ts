27d56d71433e4c11a214134e5d6c816999a3bcf9
lik3as <daviacosta2006@gmail.com>
2023-10-30 09:20:55 -0300

    docs: explaining how to use the ScopesOptionsGetter type

--- src/scopes/scope-options.ts
+++ src/scopes/scope-options.ts
@@ -13,5 +13,18 @@ export interface ScopeOptionsGetters {
 }
 
 export type DefaultScopeGetter = () => FindOptions;
+
+/**
+ * This is the type that you should use if adding a new set of scopes
+ * @example
+ * const myModelScopes: ScopesOptionsGetter = ()  => ({
+ *  scopeOne: FindOptions & IncludeOptions {
+ *    return {...}
+ *  },
+ *  scopeTwo: FindOptions {
+ *    return {...}
+ *  }
+ * })
+ */
 export type ScopesOptionsGetter = () => { [sopeName: string]: ScopesOptions };
 export type ScopesOptions = FindOptions | ((...args: any[]) => FindOptions);
